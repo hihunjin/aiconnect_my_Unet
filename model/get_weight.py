1cc254a2c18f1ac8a2cfa876fde801db89c671c9
hihunjin <32363064+hihunjin@users.noreply.github.com>
2021-06-08 17:57:15 +0900

    Update get_weight.py

--- model/get_weight.py
+++ model/get_weight.py
@@ -1,9 +1,13 @@
 import os
 
 def Get_weight():
-    os.system('pip install gdown')
+    print('Please wait : 0/3')
+    os.system('pip install gdown  > /dev/null')
+    print('Please wait : 1/3')
     if not os.path.isfile('model_best.pth.tar'):
-        os.system('gdown https://drive.google.com/uc?id=1Ysh-QhyvZ4q2Q8_zjlDo0uituARrQjWZ')
+        os.system('gdown https://drive.google.com/uc?id=1Ysh-QhyvZ4q2Q8_zjlDo0uituARrQjWZ > /dev/null')
     # os.system('apt update -y')
     # os.system('apt install git -y')
-    os.system('pip install segmentation-models-pytorch')
+    print('Please wait : 2/3')
+    os.system('pip install segmentation-models-pytorch > /dev/null')
+    print('Please wait : 3/3')
